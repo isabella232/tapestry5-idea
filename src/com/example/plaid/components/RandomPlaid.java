da563d532cc7313498726ad3bce2463f122b7197
Uriah Carpenter <uriah@widen.com>
2013-01-29 12:24:53 -0600

    Add 'alt' as a required component parameter

--- src/com/example/plaid/components/RandomPlaid.java
+++ src/com/example/plaid/components/RandomPlaid.java
@@ -3,6 +3,7 @@ package com.example.plaid.components;
 import com.example.plaid.services.PlaidService;
 import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.SupportsInformalParameters;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
@@ -16,9 +17,12 @@ public class RandomPlaid
     @Inject
     private PlaidService plaidService;
 
+    @Parameter(required = true, defaultPrefix = "literal")
+    private String alt;
+
     boolean beginRender(MarkupWriter writer)
     {
-        writer.element("img", "src", plaidService.getPlaidImagePath());
+        writer.element("img", "src", plaidService.getPlaidImagePath(), "alt", alt, "title", alt);
 
         resources.renderInformalParameters(writer);
 
