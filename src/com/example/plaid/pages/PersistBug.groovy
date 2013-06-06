9cb5be82db6ea917238aaf7e7b3f3e6067681913
Uriah Carpenter <uriah@widen.com>
2013-06-06 16:11:18 -0500

    groovy/closure/access/persist bug

---
+++ src/com/example/plaid/pages/PersistBug.groovy
@@ -0,0 +1,41 @@
+package com.example.plaid.pages
+import org.apache.tapestry5.SymbolConstants
+import org.apache.tapestry5.annotations.Persist
+import org.apache.tapestry5.ioc.annotations.Inject
+import org.apache.tapestry5.ioc.annotations.Symbol
+
+class PersistBug
+{
+
+    @Inject
+    @Symbol(SymbolConstants.PRODUCTION_MODE)
+    boolean productionModeEnabled
+
+    // workaround A -- change words field from 'private' to 'default' (no modifier) access
+    @Persist
+    private List<String> words
+
+    // workaround B -- create a 'getter' method; as below
+    //private List<String> getWords()
+    //{
+    //    return words
+    //}
+
+    // bug -- with ProductionMode enabled 'words' will be unexpectedly NULL inside the closure
+    String getOutput()
+    {
+        words = []
+
+        [ 'Apple', 'Banana', 'Grape', 'Orange' ].each {
+            if (it == 'Grape') {
+                words << 'Wine'
+            }
+            else {
+                words << it
+            }
+        }
+
+        return words.join('\n')
+    }
+
+}
